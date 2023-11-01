e7492b56f9b3b5af330e86e1d76f201077ea3cb9
Khrystyna Klapushchak <klapushchak.khr@gmail.com>
2023-11-01 14:00:40 -0400

    complete

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import {Link} from 'react-router-dom';
 
-function MovieCard({title}) {
+function MovieCard({mov}) {
+  console.log(mov)
   
   return (
     <article>
-        <h2>{title}</h2>
-        {/* What should go here? */}
+        <h2>{mov.title}</h2>
+        <Link to={`/movie/${mov.id}`}>View movie</Link>
     </article>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,27 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import MovieCard from "../components/MovieCard";
 
 function Home() {
+  const [movies, setMovies]=useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:4000/movies')
+    .then(res=>res.json())
+    .then(data=>{setMovies(data)})
+  }, [])
+
+  console.log(movies)
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar/>
       </header>
       <main>
-        {/* Info goes here! */}
+        <h1>Home page</h1>
+        {movies.map(mov=>{
+          return <MovieCard mov={mov} key={mov.title}/>
+        })}
       </main>
     </>
   );
